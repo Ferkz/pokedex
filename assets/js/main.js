dfa0cf341782a9fe92fe15cc941d18a6f3aaaf4e
Ferkz <fermon1952@gmail.com>
2023-08-28 12:38:36 -0400

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
 const offset = 0;
 const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=10"`;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
 fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody)) 
+    .catch((error) => console.log(error))
+    .finally(()=> console.log('tudo ok'))
